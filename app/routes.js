d9a41893bd3a754612aee09e6c0075c4d2fe02ad
Kaysen-Kang <kaysenkang@gmail.com>
2014-05-30 18:17:18 +0800

    update router

--- app/routes.js
+++ app/routes.js
@@ -26,7 +26,7 @@ module.exports = function(app, passport) {
 
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
-        res.redirect('bullhorn');
+        res.render('index.ejs');
     });
 
     // BULLHORN SECTION =========================
